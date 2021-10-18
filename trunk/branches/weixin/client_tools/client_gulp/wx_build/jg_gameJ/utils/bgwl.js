2ebb42723d51988b8f661f36bd212339e2a9b55c
zkh <enter334714@163.com>
2021-10-18 15:42:40 +0800

    替换参数

--- trunk/branches/weixin/client_tools/client_gulp/wx_build/jg_gameJ/utils/bgwl.js
+++ trunk/branches/weixin/client_tools/client_gulp/wx_build/jg_gameJ/utils/bgwl.js
@@ -1,18 +1,18 @@
 module.exports = {
   //正式环境参数
   pro: {
-    appid: "wx5cc7c8278836be4b",
-    secret_key: "sec11be4254418a5ee0",  //分配的加密串，非微信appsecret
-    identify: "ide7363c57c34e8d596",   //游戏标识
+    appid: "wx538953a4f1a3a954",
+    secret_key: "sec6d62d171239f11cb",  //分配的加密串，非微信appsecret
+    identify: "idebf46f44f8912c2af",   //游戏标识
     sign_type: "sha1",      //枚举，可选 sha1、md5
     channel_id: 'test_channel_id',  //无明确要求，不需要修改
     sub_channel_id: 'test_sub_channel_id2' //无明确要求，不需要修改
   },
   //测试环境参数
   devp: {
-    appid: "wx5cc7c8278836be4b",
-    secret_key: "seccaab8a47e4d3c31d",  //分配的加密串，非微信appsecret
-    identify: "ide14f480e1ec5a2b48", //游戏标识
+    appid: "wx538953a4f1a3a954",
+    secret_key: "sec7c82ccabf981fc37",  //分配的加密串，非微信appsecret
+    identify: "ide20903e1cfe126d52", //游戏标识
     sign_type: "sha1",      //枚举，可选 sha1、md5
     channel_id: 'test_channel_id',  //无明确要求，不需要修改
     sub_channel_id: 'test_sub_channel_id2' //无明确要求，不需要修改
