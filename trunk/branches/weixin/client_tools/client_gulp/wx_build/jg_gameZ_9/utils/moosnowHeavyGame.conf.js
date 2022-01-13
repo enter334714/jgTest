7346361bd40938293ead0e247e2484e1ff9e810a
zkh <enter334714@163.com>
2022-01-13 16:59:10 +0800

    z9初始包

---
+++ trunk/branches/weixin/client_tools/client_gulp/wx_build/jg_gameZ_9/utils/moosnowHeavyGame.conf.js
@@ -0,0 +1,11 @@
+
+window.moosnowHeavyGameConfig = {
+    nowPlatform: "wx",
+    /**
+     * 微信平台
+     */
+    wx: {
+        moosnowAppId: "wx8dcbd9158bf16cbc",
+        version: "1.0.0",
+    }
+}
