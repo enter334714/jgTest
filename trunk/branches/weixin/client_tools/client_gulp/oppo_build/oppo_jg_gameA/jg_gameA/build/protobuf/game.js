ec4ff3d274480d3027f22188b572700a85966b2b
zkh <enter334714@163.com>
2023-05-18 12:04:23 +0800

    oppop不支持 这种 module module.export

--- trunk/branches/weixin/client_tools/client_gulp/oppo_build/oppo_jg_gameA/jg_gameA/build/protobuf/game.js
+++ trunk/branches/weixin/client_tools/client_gulp/oppo_build/oppo_jg_gameA/jg_gameA/build/protobuf/game.js
@@ -2,5 +2,6 @@ require("protobuf/test.js");
 require("protobuf/protobuf.js");
 console.log("windowprotobuf5:",window["protobuf"]);
 window["protobuf"].util.Long = null;
-window["client_pb"] = require("protobuf/client_pb.js");
+require("protobuf/client_pb.js");
+console.log(window["client_pb"]);
 window["protobufRoot"] = window["protobuf"].Root.fromJSON(window["client_pb"]);
--- trunk/branches/weixin/client_tools/client_gulp/oppo_build/oppo_jg_gameA/jg_gameA/protobuf/client_pb.js
+++ trunk/branches/weixin/client_tools/client_gulp/oppo_build/oppo_jg_gameA/jg_gameA/protobuf/client_pb.js
@@ -1,4 +1,4 @@
-module.exports = {
+window["client_pb"] = {
   "nested": {
     "ProtocolClient": {
       "nested": {
