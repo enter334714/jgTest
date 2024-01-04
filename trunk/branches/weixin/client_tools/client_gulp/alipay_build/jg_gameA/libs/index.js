dfae583a5de0a969091e11d9b9a4f533144956c7
zkh <enter334714@163.com>
2024-01-04 16:51:59 +0800

    fox

--- trunk/branches/weixin/client_tools/client_gulp/alipay_build/jg_gameA/libs/index.js
+++ trunk/branches/weixin/client_tools/client_gulp/alipay_build/jg_gameA/libs/index.js
@@ -20,24 +20,7 @@ PF_INFO.package = "0";
 PF_INFO.version = window.versions.wxVersion;
 PF_INFO.mac = "";
 PF_INFO.os = "1";
-PF_INFO.sdk_name = "9130";
-PF_INFO.apiurl = "https://api-tjqytest.sh9130.com";
-PF_INFO.logurl = "https://log-tjqytest.sh9130.com";
-PF_INFO.payurl = "https://pay-tjqytest.sh9130.com";
-PF_INFO.apikey = "MQx0mYlUWO5XYKvgAIPKWgK1w722GKih";
-PF_INFO.partnerId = "1";
-PF_INFO.pkgName = "";
-PF_INFO.device_id = "";
-PF_INFO.from_scene = 0;
-PF_INFO.serverList = {};
-PF_INFO.channelNum = parseInt(PF_INFO.partnerId);
-PF_INFO.channel = PF_INFO.partnerId;
-PF_INFO.selectedServer = {};
 
-
-PF_INFO.showLogo = false;
-PF_INFO.debugUsers = "39927500|58163716|74597555|172978096"; //平台账号
-PF_INFO.tick = Date.now();
 
 
 
