8f89fe8619f5c22ed0a0bfc452263021f215f8fa
zkh <enter334714@163.com>
2024-01-04 16:53:03 +0800

    fix

--- trunk/branches/weixin/client_tools/client_gulp/alipay_build/jg_gameA/libs/index.js
+++ trunk/branches/weixin/client_tools/client_gulp/alipay_build/jg_gameA/libs/index.js
@@ -11,15 +11,6 @@ window.PACK = true;
 window.WSS = true;
 window.workerJsURL = "";
 window.isWaiFangWx = false;
-window.PF_INFO = {
-  base_cdn: "https://cdn-tjqy.sh9130.com/weixin_0/",
-  cdn: "https://cdn-tjqy.sh9130.com/weixin_0/",
-}
-PF_INFO.pay_infos = {}
-PF_INFO.package = "0";
-PF_INFO.version = window.versions.wxVersion;
-PF_INFO.mac = "";
-PF_INFO.os = "1";
 
 
 
